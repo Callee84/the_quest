d5d1f9889a03d3cb455b9e5e8f6b5f47c866f9d0
Callee84 <carl.g.holm@gmail.com>
2022-10-06 09:27:47 +0000

    Creating while loop to start game with two chioces ans a else statement for looping back when entering a false character

--- run.py
+++ run.py
@@ -10,3 +10,17 @@ print("This is tale about a exiting journey across the mythical lands of PythWor
 print("You can cross path with some intresting beeings and will have to make some hard choices along your choosen path.")
 print("But be aware... Your choices has consequences!\n")
 print("HINT - Be sure to type in your choosen answere who is provided for you in (bracets)")
+
+while True:
+    user_input = input(
+        "Let's go for a walk! You never know what you might find. Ready? ").lower()
+    if user_input == "yes":
+        print("Lovely! Let's Go!")
+        break
+
+    elif user_input == "no":
+        print("Are you sure? It's really a lovely day!")
+        continue
+    else:
+        print("Sorry, I didn't catch that: Could you type again?")
+        continue
