735065ca4cd588e6fcc9884583715efacf17e871
Callee84 <carl.g.holm@gmail.com>
2022-10-06 09:47:13 +0000

    Adding first step on the quest with if statements

--- run.py
+++ run.py
@@ -24,3 +24,12 @@ while True:
     else:
         print("Sorry, I didn't catch that: Could you type again?")
         continue
+
+choiceOne = input(
+    "You come to a river. There's a man guarding the only bridge. Do you (talk) to the guard or du you try to (sneak) past? ").lower()
+if choiceOne == "talk":
+    print("The guard seems to be in a good mood today and let you pass. But before he let you go, he gave you a knife and told you 'Stay away from the beach. It's dangerous!")
+
+elif choiceOne == "sneak":
+    print("Nice call... You were able to sneak past the guard.")
+    answer = input("You were able to dodge the guard and sneek over to the other side. \nIf you look to the right it's a nice (beach) and to your left there is a (forest). \nWhere do you go? ")
