a736fb2096f90b947b423f7cbf8ce5fb80d99480
Callee84 <carl.g.holm@gmail.com>
2022-10-06 09:01:02 +0000

    Initial commit with the start of the quest

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+"""
+The quest is a multiple chioce game where the user make choice along the way.
+"""
+
+name = input("What's your name? ")
+print("Welcome", name, "!\n")
+
+print("This is tale about a exiting journey across the mythical lands of PythWorld.")
+print("You can cross path with some intresting beeings and will have to make some hard choices along your choosen path.")
+print("But be aware... Your choices has consequences!\n")
+print("HINT - Be sure to type in your choosen answere who is provided for you in (bracets)")
